a14f61641fe7f7457c6dd80edac28b4e42b94513
Amirulll <33915660+fitriamirul15@users.noreply.github.com>
2018-01-18 10:18:59 +0800

    lab 6 update

--- Lab-Exercise-6.cpp
+++ Lab-Exercise-6.cpp
@@ -1,12 +1,26 @@
 //convert the program below to a "do...while" loop
 
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
 int n;
+
+do{
 cout<<"Enter a non-negative integer: ";
 cin>>n;
 
-while(n<0)
+}while(n<0);
 {
+
 	cout<<"The integer your entered is negative."<<endl;
 	cout<<"Enter a non-negative integer";
 	cin>>n;
+
+}
+
 }
